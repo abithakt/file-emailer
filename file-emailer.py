f8357f46e7265c058a2ea30131d76668f47532ec
Abitha <abithathyagarajan@gmail.com>
2018-05-02 22:43:19 +0530

    Remove unnecessary modules and commented code

--- file-emailer.py
+++ file-emailer.py
@@ -1,14 +1,7 @@
 import ruamel.yaml as yaml
 import smtplib
 import gnupg
-#import mimetypes
 from sys import argv
-from subprocess import run
-
-#from email.message import Message
-#from email.header import Header
-#from email.encoders import encode_base64
-#from email.utils import formatdate, make_msgid
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -24,27 +17,14 @@ gpg = gnupg.GPG(
     keyring=config['keyring'],
     secring=config['secring'])
 
-#f = argv[1]
-#thefile = open(f)
-#pasph = input('passphrase: ')
-
 encryptedemail = MIMEMultipart('alternative')
-encryptedemail['Subject'] = "test"
+encryptedemail['Subject'] = argv[1]
 encryptedemail['From'] = config['sender']
 encryptedemail['To'] = config['recipient']
-#email_text = MIMEText(gpg.encrypt(thefile.read().encode('ascii'), config['recipient'], config['sender-key'], pasph))
-#email_text = MIMEText(gpg.encrypt(str(thefile.read()).encode('ascii'), config['sender']))
-
-"""def encrypt(filename):
-    result = run("gpg -es " + filename, stdout=subprocess.PIPE, input=config['passphrase'])
-    return result.stdout.decode('utf-8')"""
-
 
 with open(argv[1]) as thefile:
-    #email_text = MIMEText(thefile.read()
-    #sender.sendmail(email_text, config['sender'], config['sender'])
     email_text = MIMEText(str(gpg.encrypt(
-        thefile.read(),
+        thefile.read(), # message content
         config['recipient-key'],
         default_key=config['sender-key'],
         passphrase=config['passphrase'],
@@ -53,6 +33,6 @@ with open(argv[1]) as thefile:
     )), 'plain')
     encryptedemail.attach(email_text)
 
-    sender.send_message(encryptedemail)
+sender.send_message(encryptedemail)
 
 sender.quit()
