2130e2c4bac6032296573aeeb09fdc7ffe4f3e34
Abitha <abithathyagarajan@gmail.com>
2018-05-07 17:46:20 +0530

    Add license and author info

--- file-emailer.py
+++ file-emailer.py
@@ -1,3 +1,21 @@
+# file-emailer: Email a file to yourself
+# Author: Abitha K Thyagarajan <abitha@pm.me>
+# Copyright (C) 2018 Abitha K Thyagarajan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import ruamel.yaml as yaml
 import smtplib
 import gnupg
